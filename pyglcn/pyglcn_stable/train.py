25ade8dfb3f7af83af6dd79e0eb298dfb503de07
Ci Nie <codezzzsleep@163.com>
2023-11-07 02:02:22 +0800

    runs1

--- pyglcn/pyglcn_stable/train.py
+++ pyglcn/pyglcn_stable/train.py
@@ -4,6 +4,7 @@ import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim import lr_scheduler
 
 from utils import load_data, accuracy, same_seed
 from models import GLCN, GCN
@@ -11,7 +12,7 @@ import matplotlib.pyplot as plt
 from GLoss import gl_loss
 from torch.utils.tensorboard import SummaryWriter
 
-writer = SummaryWriter("../logs")
+writer = SummaryWriter("../logs/run1")
 loss_list = []
 test_acc_list = []
 # Training settings
@@ -20,8 +21,8 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='Disables CUDA training.')
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
-parser.add_argument('--seed', type=int, default=123, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=600,
+parser.add_argument('--seed', type=int, default=3407, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=1000,
                     help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.01,
                     help='Initial learning rate.')
@@ -52,8 +53,10 @@ model = GLCN(in_dim=features.shape[1],
              hidden_gcn=30,
              out_dim=labels.max().item() + 1,
              dropout=args.dropout)
+# print(model.gl.weights)
 optimizer = optim.Adam(model.parameters(),
                        lr=args.lr, weight_decay=args.weight_decay)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 model_ = GCN(in_dim=features.shape[1],
              hidden_dim=30,
              out_dim=labels.max().item() + 1,
@@ -63,6 +66,7 @@ optimizer_ = optim.Adam(model_.parameters(),
 loss_fn = nn.CrossEntropyLoss()
 if args.cuda:
     model.cuda()
+    model_.cuda()
     features = features.cuda()
     adj = adj.cuda()
     edge = edge.cuda()
@@ -75,27 +79,64 @@ if args.cuda:
 def train(epoch):
     t = time.time()
     model.train()
+    model_.train()
     optimizer.zero_grad()
+    optimizer_.zero_grad()
     x, adj_new, output = model(features, edge)
+    output_ = model_(features, adj)
+    L2_loss1 = torch.norm(model.gl.weights, p=2)
+    L2_loss2 = torch.norm(model.gcn1.weight, p=2)
     loss_train = loss_fn(output[idx_train], labels[idx_train])
-    writer.add_scalar("CrossEntropyLoss", loss_train, epoch + 1)
+    loss_train_ = loss_fn(output_[idx_train], labels[idx_train])
+
+    writer.add_scalars(main_tag="CrossEntropyLoss",
+                       tag_scalar_dict={
+                           "glcn": loss_train,
+                           "gcn": loss_train_
+                       },
+                       global_step=epoch + 1)
+
     gl_loss_train = gl_loss(x, adj_new, args.losslr1, args.losslr2)
     writer.add_scalar("GLoss", gl_loss_train, epoch + 1)
-    loss_train = gl_loss_train + loss_train
-    writer.add_scalar("TotalLoss", loss_train, epoch + 1)
+    loss_train = gl_loss_train + loss_train + L2_loss1 * args.weight_decay + L2_loss2 * args.weight_decay
+    writer.add_scalars(main_tag="TotalLoss",
+                       tag_scalar_dict={
+                           "glcn": loss_train,
+                           "gcn": loss_train_
+                       },
+                       global_step=epoch + 1)
     loss_list.append(loss_train.item())
     acc_train = accuracy(output[idx_train], labels[idx_train])
-    writer.add_scalar("Train-Acc", acc_train, epoch + 1)
+    acc_train_ = accuracy(output_[idx_train], labels[idx_train])
+    writer.add_scalars(main_tag="Train-Acc",
+                       tag_scalar_dict={
+                           "glcn": acc_train,
+                           "gcn": acc_train_
+                       },
+                       global_step=epoch + 1)
     loss_train.backward()
+    loss_train_.backward()
     optimizer.step()
+    optimizer_.step()
 
     model.eval()
     x, adj_new, output = model(features, edge)
+    output_ = model_(features, adj)
+
     loss_val = loss_fn(output[idx_val], labels[idx_val])
+    loss_val_ = loss_fn(output_[idx_val], labels[idx_val])
+
     gl_loss_val = gl_loss(x, adj_new, args.losslr1, args.losslr2)
     loss_val += gl_loss_val
     acc_val = accuracy(output[idx_val], labels[idx_val])
-    writer.add_scalar("Val-Acc", acc_val, epoch + 1)
+    acc_val_ = accuracy(output_[idx_val], labels[idx_val])
+
+    writer.add_scalars(main_tag="Val-Acc",
+                       tag_scalar_dict={
+                           "glcn": acc_val,
+                           "gcn": acc_val_
+                       },
+                       global_step=epoch + 1)
     print('Epoch: {:04d}'.format(epoch + 1),
           'loss_train: {:.4f}'.format(loss_train.item()),
           'acc_train: {:.4f}'.format(acc_train.item()),
@@ -105,49 +146,60 @@ def train(epoch):
     test(epoch)
 
 
-def train_(epoch):
-    t = time.time()
-    model_.train()
-    optimizer_.zero_grad()
-    output = model_(features, adj)
-    loss_train = loss_fn(output[idx_train], labels[idx_train])
-    acc_train = accuracy(output[idx_train], labels[idx_train])
-    loss_train.backward()
-    optimizer_.step()
-
-    model_.eval()
-    output = model_(features, adj)
-
-    loss_val = loss_fn(output[idx_val], labels[idx_val])
-    acc_val = accuracy(output[idx_val], labels[idx_val])
-    print('Epoch: {:04d}'.format(epoch + 1),
-          'loss_train: {:.4f}'.format(loss_train.item()),
-          'acc_train: {:.4f}'.format(acc_train.item()),
-          'loss_val: {:.4f}'.format(loss_val.item()),
-          'acc_val: {:.4f}'.format(acc_val.item()),
-          'time: {:.4f}s'.format(time.time() - t))
-    test_()
-
-
-def test_():
-    model_.eval()
-    output = model_(features, adj)
-    loss_test = loss_fn(output[idx_test], labels[idx_test])
-    acc_test = accuracy(output[idx_test], labels[idx_test])
-    print("Test set results:",
-          "loss= {:.4f}".format(loss_test.item()),
-          "accuracy= {:.4f}".format(acc_test.item()))
+# def train_(epoch):
+#     t = time.time()
+#     model_.train()
+#     optimizer_.zero_grad()
+#     output = model_(features, adj)
+#     loss_train = loss_fn(output[idx_train], labels[idx_train])
+#     acc_train = accuracy(output[idx_train], labels[idx_train])
+#     loss_train.backward()
+#     optimizer_.step()
+#
+#     model_.eval()
+#     output = model_(features, adj)
+#
+#     loss_val = loss_fn(output[idx_val], labels[idx_val])
+#     acc_val = accuracy(output[idx_val], labels[idx_val])
+#     print('Epoch: {:04d}'.format(epoch + 1),
+#           'loss_train: {:.4f}'.format(loss_train.item()),
+#           'acc_train: {:.4f}'.format(acc_train.item()),
+#           'loss_val: {:.4f}'.format(loss_val.item()),
+#           'acc_val: {:.4f}'.format(acc_val.item()),
+#           'time: {:.4f}s'.format(time.time() - t))
+#     test_()
+#
+#
+# def test_():
+#     model_.eval()
+#     output = model_(features, adj)
+#     loss_test = loss_fn(output[idx_test], labels[idx_test])
+#     acc_test = accuracy(output[idx_test], labels[idx_test])
+#     print("Test set results:",
+#           "loss= {:.4f}".format(loss_test.item()),
+#           "accuracy= {:.4f}".format(acc_test.item()))
 
 
 def test(epoch):
     model.eval()
+    model_.eval()
     x, adj_new, output = model(features, edge)
+    output_ = model_(features, adj)
+
     loss_test = loss_fn(output[idx_test], labels[idx_test])
+    loss_test_ = loss_fn(output_[idx_test], labels[idx_test])
+
     gl_loss_test = gl_loss(x, adj_new, args.losslr1, args.losslr2)
     loss_test += gl_loss_test
     acc_test = accuracy(output[idx_test], labels[idx_test])
+    acc_test_ = accuracy(output_[idx_test], labels[idx_test])
     test_acc_list.append(acc_test)
-    writer.add_scalar("Test-Acc", acc_test, epoch + 1)
+    writer.add_scalars(main_tag="Test-Acc",
+                       tag_scalar_dict={
+                           "glcn": acc_test,
+                           "gcn": acc_test_
+                       },
+                       global_step=epoch + 1)
     print("Test set results:",
           "loss= {:.4f}".format(loss_test.item()),
           "accuracy= {:.4f}".format(acc_test.item()))
@@ -157,7 +209,7 @@ def test(epoch):
 t_total = time.time()
 for epoch in range(args.epochs):
     train(epoch)
-    train_(epoch)
+    # train_(epoch)
 writer.close()
 print("Optimization Finished!")
 print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
